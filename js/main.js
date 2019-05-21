71e61472ab4bca4a2b2ebd5254b286d7f13a062a
Mambli-Joe <akwa_ash@mail.ru>
2019-05-21 21:28:15 +0400

    works section

--- js/main.js
+++ js/main.js
@@ -35,4 +35,17 @@ $('li').on('click', function (event) {
     });
     
     //WOW ANIMATE
-	new WOW().init();
+    new WOW().init();
+    
+    //WORKS
+    $('.portfolio__toggles button').click(function(){
+        var get_id = this.id;
+        var get_current = $('.portfolio__posts .' + get_id);
+    
+        $('.post').not(get_current).hide(500);
+        get_current.show(500);
+       });
+    
+       $('#showall').click(function() {
+        $('.post').show(500);
+       });
